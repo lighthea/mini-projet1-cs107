9c4a0d3ca0002da701546182bab3bd7e3f8d23ea
Alex <alex.sallinen@gmail.com>
2018-10-31 16:11:18 +0100

    UPdate on Kmeans

--- src/cs107KNN/KMeansClustering.java
+++ src/cs107KNN/KMeansClustering.java
@@ -1,30 +1,32 @@
 package cs107KNN;
 
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
+
 
 public class KMeansClustering {
 	public static void main(String[] args) {
-		int K = 5000;
-		int maxIters = 20;
+		int K = 10;
+		int maxIters = 5;
 
-		// TODO: Adaptez les parcours
-		byte[][][] images = KNN.parseIDXimages(Helpers.readBinaryFile("TODO_remplacer/1000-per-digit_images_train"));
-		byte[] labels = KNN.parseIDXlabels(Helpers.readBinaryFile("TODO_remplacer/1000-per-digit_labels_train"));
+		byte[][][] images = KNN.parseIDXimages(Helpers.readBinaryFile("datasets/Test/10k_images_test"));
+		byte[] labels = KNN.parseIDXlabels(Helpers.readBinaryFile("datasets/Test/10k_labels_test"));
 
 		byte[][][] reducedImages = KMeansReduce(images, K, maxIters);
 
-		byte[] reducedLabels = new byte[reducedImages.length];
+		byte[] reducedLabels
+                = new byte[reducedImages.length];
 		for (int i = 0; i < reducedLabels.length; i++) {
 			reducedLabels[i] = KNN.knnClassify(reducedImages[i], images, labels, 5);
 			System.out.println("Classified " + (i + 1) + " / " + reducedImages.length);
 		}
 
+		Helpers.show("Titre", reducedImages, reducedLabels, 10, 10);
 		Helpers.writeBinaryFile("datasets/reduced10Kto1K_images", encodeIDXimages(reducedImages));
 		Helpers.writeBinaryFile("datasets/reduced10Kto1K_labels", encodeIDXlabels(reducedLabels));
+
 	}
+
 
     /**
      * @brief Encodes a tensor of images into an array of data ready to be written on a file
@@ -34,6 +36,21 @@ public class KMeansClustering {
      * @return the array of byte ready to be written to an IDX file
      */
 	public static byte[] encodeIDXimages(byte[][][] images) {
+        int depth = images.length,imageWidth = images[0][0].length, imageHeight = images[0].length;
+
+	    byte [] imageIDX = new byte[16 + depth * imageHeight * imageWidth];
+        encodeInt(2051, imageIDX, 0);
+        encodeInt(images.length, imageIDX, 4);
+        encodeInt(images[0].length, imageIDX, 8);
+        encodeInt(images[0][0].length, imageIDX, 12);
+
+		for (int row = 0, image = 0, pixel = 16;
+             image < depth;
+             pixel += imageWidth, image += (row+1)/imageHeight, row = (row+1)%imageHeight) {
+
+		    System.arraycopy(images[image][row], 0, imageIDX, pixel, imageWidth);
+		}
+        return imageIDX;
 
 	}
 
@@ -45,21 +62,28 @@ public class KMeansClustering {
      * @return the array of bytes ready to be written to an IDX file
      */
 	public static byte[] encodeIDXlabels(byte[] labels) {
-		// TODO: Implémenter
-		return null;
+		byte [] labelIDX = new byte[8 + labels.length];
+	    encodeInt(2049, labelIDX, 0);
+        encodeInt(labels.length, labelIDX, 4);
+        System.arraycopy(labels, 0, labelIDX, 8, labels.length);
+
+		return labelIDX;
 	}
 
     /**
      * @brief Decomposes an integer into 4 bytes stored consecutively in the destination
      * array starting at position offset
-     * 
      * @param n the integer number to encode
      * @param destination the array where to write the encoded int
      * @param offset the position where to store the most significant byte of the integer,
      * the others will follow at offset + 1, offset + 2, offset + 3
      */
 	public static void encodeInt(int n, byte[] destination, int offset) {
-		// TODO: Implémenter
+
+	    destination[offset] = (byte)(n >>> 24);
+	    destination[offset+1] = (byte)(n >>> 16);
+	    destination[offset+2] = (byte)(n >>> 8);
+	    destination[offset+3] = (byte)(n);
 	}
 
     /**
@@ -71,27 +95,29 @@ public class KMeansClustering {
      * 
      * @return the tensor containing the reduced dataset
      */
-	public static byte[][][] KMeansReduce(byte[][][] tensor, int size, int maxIters) {
-		int[] assignments = new Random().ints(tensor.length, 0, size).toArray();
-		byte[][][] centroids = new byte[size][][];
-		initialize(tensor, assignments, centroids);
+    public static byte[][][] KMeansReduce(byte[][][] tensor, int size, int maxIters) {
+        int[] assignments = new Random().ints(tensor.length, 0, size).toArray();
+        byte[][][] centroids = new byte[size][][];
+        initialize(tensor, assignments, centroids);
 
-		int nIter = 0;
-		while (nIter < maxIters) {
-			// Step 1: Assign points to closest centroid
-			recomputeAssignments(tensor, centroids, assignments);
-			System.out.println("Recomputed assignments");
-			// Step 2: Recompute centroids as average of points
-			recomputeCentroids(tensor, centroids, assignments);
-			System.out.println("Recomputed centroids");
+        int nIter = 0;
 
-			System.out.println("KMeans completed iteration " + (nIter + 1) + " / " + maxIters);
 
-			nIter++;
-		}
+        while (nIter < maxIters) {
+            // Step 1: Assign points to closest centroid
+            recomputeAssignments(tensor, centroids, assignments);
+            System.out.println("Recomputed assignments");
+            // Step 2: Recompute centroids as average of points
+            recomputeCentroids(tensor, centroids, assignments);
+            System.out.println("Recomputed centroids");
 
-		return centroids;
-	}
+            System.out.println("KMeans completed iteration " + (nIter + 1) + " / " + maxIters);
+
+            nIter++;
+        }
+
+        return centroids;
+    }
 
    /**
      * @brief Assigns each image to the cluster whose centroid is the closest.
@@ -103,6 +129,18 @@ public class KMeansClustering {
      *  if j is at position i, then image i belongs to cluster j
      */
 	public static void recomputeAssignments(byte[][][] tensor, byte[][][] centroids, int[] assignments) {
+	    assert assignments.length == tensor.length;
+        float temp, temp2;
+
+        for (int i = 0; i < assignments.length; i++) {
+            for (int j = 0; j < centroids.length; j++) {
+                temp = KNN.squaredEuclideanDistance(tensor[i], centroids[j]);
+                temp2 = KNN.squaredEuclideanDistance(tensor[i], centroids[assignments[i]]);
+                if (temp < temp2){
+                    assignments[i] = j;
+                }
+            }
+        }
 	}
 
     /**
@@ -113,9 +151,19 @@ public class KMeansClustering {
      * @param assignments the vector indicating to what cluster each image belongs to.
      *  if j is at position i, then image i belongs to cluster j
      */
-	public static void recomputeCentroids(byte[][][] tensor, byte[][][] centroids, int[] assignments) {
-	}
-
+    public static void recomputeCentroids(byte[][][] tensor/*images*/, byte[][][] centroids/*imagesmoyennes*/, int[] assignments) {
+        centroids = new byte[centroids.length][centroids[0].length][centroids[0][0].length];
+        float compteur = 0;
+        for (int i = 0; i < assignments.length; i++)/*pour chaque image d'indice i*/ {
+            for (int u = 0; u < centroids[0].length; u++)/*pour chaque ligne*/ {
+                for (int v = 0; v < centroids[0][0].length; v++)/*pour chaque colonne*/  {
+                    compteur += 1;
+                    centroids[assignments[i]][u][v] += (1 / (compteur) * (tensor[assignments[i]][u][v] & 0xFF));
+                }
+            }
+        compteur = 0;
+        }
+    }
     /**
      * Initializes the centroids and assignments for the algorithm.
      * The assignments are initialized randomly and the centroids
@@ -126,15 +174,15 @@ public class KMeansClustering {
      * @param centroids the tensor of centroids that represent the cluster of images
      *  if j is at position i, then image i belongs to cluster j
      */
-	public static void initialize(byte[][][] tensor, int[] assignments, byte[][][] centroids) {
-		Set<Integer> centroidIds = new HashSet<>();
-		Random r = new Random("cs107-2018".hashCode());
-		while (centroidIds.size() != centroids.length)
-			centroidIds.add(r.nextInt(tensor.length));
-		Integer[] cids = centroidIds.toArray(new Integer[] {});
-		for (int i = 0; i < centroids.length; i++)
-			centroids[i] = tensor[cids[i]];
-		for (int i = 0; i < assignments.length; i++)
-			assignments[i] = cids[r.nextInt(cids.length)];
-	}
+    public static void initialize(byte[][][] tensor, int[] assignments, byte[][][] centroids) {
+        Set<Integer> centroidIds = new HashSet<>();
+        Random r = new Random("cs107-2018".hashCode());
+        while (centroidIds.size() != centroids.length)
+            centroidIds.add(r.nextInt(tensor.length));
+        Integer[] cids = centroidIds.toArray(new Integer[] {});
+        for (int i = 0; i < centroids.length; i++)
+            centroids[i] = tensor[cids[i]];
+        for (int i = 0; i < assignments.length; i++)
+            assignments[i] = new Random().nextInt(centroids.length);
+    }
 }
--- src/cs107KNN/KNN.java
+++ src/cs107KNN/KNN.java
@@ -2,30 +2,37 @@ package cs107KNN;
 
 import java.util.Arrays;
 
+
+
 public class KNN {
 
     public static void main(String[] args) {
-        int testlines = 10;
-        int testcolumns = 10;
-        int test = testlines * testcolumns;
-        int K =7;
 
-        byte[][][] trainImages = parseIDXimages(Helpers.readBinaryFile("D:\\DEVELOPPEMENT\\Projects\\knn\\mini-projet1-cs107\\datasets\\Train\\5000-per-digit_images_train"));
-        byte[] trainLabels = parseIDXlabels(Helpers.readBinaryFile("D:\\DEVELOPPEMENT\\Projects\\knn\\mini-projet1-cs107\\datasets\\Train\\5000-per-digit_labels_train"));
-        byte[][][] testImages = parseIDXimages(Helpers.readBinaryFile("D:\\DEVELOPPEMENT\\Projects\\knn\\mini-projet1-cs107\\datasets\\Test\\10k_images_test"));
-        byte[] testLabels = parseIDXlabels(Helpers.readBinaryFile("D:\\DEVELOPPEMENT\\Projects\\knn\\mini-projet1-cs107\\datasets\\Test\\10k_labels_test"));
+        int columns = 12, lines = 10;
+        int test = columns * lines;
+        int K = 10;
+
+        byte[][][] trainImages = parseIDXimages(Helpers.readBinaryFile("datasets/reduced10Kto1K_images"));
+        System.out.println(trainImages.length);
+        //Helpers.show("Test", trainImages, trainImages.length,1);
+        byte[] trainLabels = parseIDXlabels(Helpers.readBinaryFile("datasets/reduced10Kto1K_labels"));
+        byte[][][] testImages = parseIDXimages(Helpers.readBinaryFile("datasets/Test/10k_images_test"));
+        byte[] testLabels = parseIDXlabels(Helpers.readBinaryFile("datasets/Test/10k_labels_test"));
         byte[] predictions = new byte[test];
 
-
         long start = System.currentTimeMillis();
+
         for(int i =0; i< test; ++i) {
             predictions[i] = knnClassify(testImages[i],trainImages, trainLabels, K);
         }
+
         double time = (System.currentTimeMillis() - start)/1000d;
+
         System.out.println("Accuracy : " + accuracy(predictions, Arrays.copyOfRange(testLabels, 0, test)));
         System.out.println("Time : " + time + " seconds !");
         System.out.println("Time per image : " + time/ test + " seconds !");
-        Helpers.show("test", testImages, predictions, testLabels, testlines,testcolumns);
+        Helpers.show("test", testImages, predictions, testLabels, lines, columns);
+
     }
 
     public static int extractInt(byte b31ToB24, byte b23ToB16, byte b15ToB8, byte b7ToB0) {
@@ -42,25 +49,22 @@ public class KNN {
 
         return b7ToB0 & 0xFF | (b15ToB8 & 0xFF) << 8 | (b23ToB16 & 0xFF) << 16 | (b31ToB24 & 0xFF) << 24;
     }
-
+    //squaredEuclideanDistance is not as optimized as invertedSimilarity since our program does not make use of it.
     public static float squaredEuclideanDistance( byte[][] a, byte[][] b) {
-        double sum = 0;
-        /*
+        float sum = 0;
+
         //We should only compare to similar matrices
         assert a.length == b.length && a[0].length == b[0].length;
-        //Separating length declaration from the loop reduces the overhead of getting the length of the array
+
         int lenI = a.length, lenJ = a[0].length;
-        if (a.length * a[0].length >= 1000) {
-            double[][] res = new double[lenI][lenJ];
-            Arrays.parallelSetAll(res, i -> {
-                Arrays.parallelSetAll(res[i], j -> Math.pow((a[i][j] & 0xFF + b[i][j] & 0xFF), 2));
-                return res[i];
-            });
-            sum = Arrays.stream(res)
-                    .flatMapToDouble(Arrays::stream)
-                    .sum();
-        */
-            return (float)sum;
+
+        for (int i = 0; i < lenI; i++) {
+            for (int j = 0; j < lenJ; j++) {
+                sum += Math.pow(a[i][j] - b[i][j], 2);
+            }
+        }
+
+            return sum;
         }
     /**
      * Parses an IDX file containing images
@@ -82,7 +86,7 @@ public class KNN {
         for (int i = 0; i < nImages * imageHeight * imageWidth; i++) data[i] = (byte) ((data[i] & 0xFF) - 128);
         byte[][][] tensor = new byte[nImages][imageHeight][imageWidth];
         for (int pixel = 16, row = 0, image = 0; (image < nImages); pixel += imageWidth,
-                row   = (row+1) % imageHeight, image += (row + 1)/ imageHeight) {
+                row = (row + 1) % imageHeight, image += (row + 1)/ imageHeight) {
             //Using integer division to avoid branching in critical loop for image number
 
             //Inserting the right pixel in the right place with array copy to avoid branching for rows and columns
@@ -104,7 +108,7 @@ public class KNN {
         assert extractInt(data[0], data[1], data[2], data[3]) == 2049;
 
         //reading metadata
-        int nLabels     = extractInt(data[4], data[5], data[6], data[7]);
+        int nLabels   = extractInt(data[4], data[5], data[6], data[7]);
         byte[] vector = Arrays.copyOfRange(data, 8, nLabels + 8);
         return vector;
     }
@@ -113,8 +117,11 @@ public class KNN {
 
         float average = 0;
         int lenI = a.length, lenJ = a[0].length;
+
         for (int i = 0; i < lenI; ++i)
-        for (int j = 0; j < lenJ; ++j) average += a[i][j] & 0xFF;
+            for (int j = 0; j < lenJ; ++j)
+                average += a[i][j] & 0xFF;
+
         return average / (lenI * lenJ);
         }
     /**
@@ -127,35 +134,33 @@ public class KNN {
 
     public static float invertedSimilarity(byte[][] a, byte[][] b) {
     assert (a.length == b.length) && (a[0].length == b[0].length);
+
+    float denominator;
     float numerator = 0;
-    float denominator = 0;
-    float moyenneA = average(a);
-    float moyenneB= average(b);
+
+    float moyenneA  = average(a);
+    float moyenneB  = average(b);
+
+    float moyenneAminus;
+    float moyenneBminus;
 
 
-    /***************************************************/
-    //calcul de trucdudessus
-
-    for(int i=0; i< a.length;++i) {
-        for(int j=0; j< a[0].length;++j) {
-            numerator += (a[i][j] - moyenneA) * (b[i][j] - moyenneB);
-        }
-    }
     /***************************************************/
     //calcul de trucdudessous
     float temp1 =0;
     float temp2 =0;
-    for(int i=0; i< a.length;++i) {
-        for(int j=0; j< a[0].length;++j) {
-            temp1 += Math.pow((a[i][j] - moyenneA),2);
+
+    for(int i = 0; i < a.length; ++i) {
+        for(int j = 0; j < a[0].length; ++j) {
+            moyenneAminus = a[i][j] - moyenneA;
+            moyenneBminus = b[i][j] - moyenneB;
+
+            numerator   += (moyenneAminus * moyenneBminus);
+            temp2       += Math.pow(moyenneBminus,2);
+            temp1       += Math.pow(moyenneAminus,2);
         }
     }
-    for(int i=0; i< a.length;++i) {
-        for(int j=0; j< a[0].length;++j) {
-            temp2 += Math.pow((b[i][j] - moyenneB),2);
-        }
-    }
-    denominator = (float) Math.sqrt(temp1*temp2);
+    denominator = (float) Math.sqrt(temp1 * temp2);
     /***************************************************/
 
     if(denominator == 0 ) {return 2;}
@@ -164,56 +169,62 @@ public class KNN {
 
     public static int[] quicksortIndices(float[] values) {
         int[] indices = new int[values.length];
-        for(int i =0; i<values.length; ++i) {
-            indices[i]=i;
+
+        for(int i = 0; i < values.length; ++i) {
+            indices[i] = i;
         }
-        int low =0;
-        int high = (values.length)-1;
-        quicksortIndices(values,indices,low,high);
+
+        int low  = 0;
+        int high = ( values.length ) - 1;
+
+        quicksortIndices( values, indices, low, high );
+
         return indices;
     }
 
     public static void quicksortIndices(float[] values, int[] indices, int low, int high) {
-        int l = low;
-        int h  = high;
+        int l       = low;
+        int h       = high;
         float pivot = values[low];
-        while(l<=h) {
-            if(values[l] < pivot) {
+        while( l <= h ) {
+            if( values[l] < pivot ) {
                 ++l;
             } else if(values[h] > pivot) {
                 --h;
             } else {
-                swap(l,h,values,indices);
+                swap(l, h, values, indices);
                 ++l;
                 --h;
             }
         }
-        if(low<h) {
-            quicksortIndices(values,indices,low,h);
+        if(low < h) {
+            quicksortIndices(values, indices, low, h);
         }
-        if(high>l) {
-            quicksortIndices(values,indices,l,high);
+        if(high > l) {
+            quicksortIndices(values, indices, l, high);
         }
     }
 
     public static void swap(int i, int j, float[] values, int[] indices) {
         float tmpFloat;
         int tmpInt;
-        tmpFloat = values[i];
+        tmpFloat  = values[i];
         values[i] = values[j];
-        values[j]= tmpFloat;
+        values[j] = tmpFloat;
 
-        tmpInt = indices[i];
-        indices[i]=indices[j];
-        indices[j]=tmpInt;
+        tmpInt      = indices[i];
+        indices[i]  = indices[j];
+        indices[j]  = tmpInt;
 
     }
 
     public static int indexOfMax(int[] array) {
         float[] floats = new float[array.length];
-        for(int i=0; i<array.length;++i) {
+
+        for(int i = 0; i < array.length; ++i) {
             floats[i] = array[i];
         }
+
         int[] indices = quicksortIndices(floats);
 
         return indices[array.length - 1];
@@ -225,7 +236,6 @@ public class KNN {
 
         for(int i =0; i<k; ++i) {
             ++results[labels[sortedIndices[i]]];
-
         }
 
         byte b = (byte) (indexOfMax(results) & (0xFF));
@@ -235,63 +245,74 @@ public class KNN {
     public static void swap(int i, int j, double[] values, int[] indices) {
         double tmpFloat;
         int tmpInt;
-        tmpFloat = values[i];
-        values[i] = values[j];
-        values[j]= tmpFloat;
 
-        tmpInt = indices[i];
-        indices[i]=indices[j];
-        indices[j]=tmpInt;
+        tmpFloat    = values[i];
+        values[i]   = values[j];
+        values[j]   = tmpFloat;
+
+        tmpInt      = indices[i];
+        indices[i]  = indices[j];
+        indices[j]  = tmpInt;
     }
 
     public static void quicksortIndices(double[] values, int[] indices, int low, int high) {
-        int l = low;
-        int h  = high;
+        int l   = low;
+        int h   = high;
         double pivot = values[low];
-        while(l<=h) {
+
+        while(l <= h) {
             if(values[l] < pivot) {
                 ++l;
             } else if(values[h] > pivot) {
                 --h;
             } else {
-                swap(l,h,values,indices);
+                swap(l, h, values, indices);
                 ++l;
                 --h;
             }
         }
-        if(low<h) {
-            quicksortIndices(values,indices,low,h);
+        if(low < h) {
+            quicksortIndices(values, indices, low, h);
         }
-        if(high>l) {
-            quicksortIndices(values,indices,l,high);
+        if(high > l) {
+            quicksortIndices(values, indices, l, high);
         }
 
     }
 
     public static int[] quicksortIndices(double[] values) {
-        int len = values.length;
-        int[] indices = new int[len];
-        for(int i =0; i<len; ++i) {
-            indices[i]=i;
+        assert values.length != 0;
+
+        int len         = values.length;
+        int[] indices   = new int[len];
+
+        for(int i = 0; i < len; ++i) {
+            indices[i] = i;
         }
-        int low =0;
-        int high = len-1;
-        quicksortIndices(values,indices,low,high);
+
+        int low     = 0;
+        int high    = len-1;
+
+        quicksortIndices(values, indices, low, high);
+
         return indices;
     }
 
     public static byte knnClassify(byte[][] image, byte[][][] trainImages, byte[] trainLabels, int k) {
+        assert image != null;
+
         double [] similarity = new double[trainLabels.length];
 
-        Arrays.parallelSetAll(similarity, i -> {return invertedSimilarity(image, trainImages[i]);});
+        Arrays.parallelSetAll(similarity, i -> invertedSimilarity(image, trainImages[i]));
+        assert similarity.length != 0;
         int[] sortedIndices = quicksortIndices(similarity);
 
         return electLabel(sortedIndices, trainLabels, k);
     }
 
     public static double accuracy(byte[] predictedLabels, byte[] trueLabels) {
-        int counter =0;
-        for(int i = 0; i<trueLabels.length; ++i) {
+        int counter = 0;
+        for(int i = 0; i < trueLabels.length; ++i) {
             if(predictedLabels[i]==trueLabels[i]) {
                 ++counter;
             }
